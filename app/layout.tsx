412678d74424b804aee13c27e262ff9f9c035d82
ddd490 <sabetfaiyazahmed1@gmail.com>
2026-02-08 21:28:25 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Press_Start_2P } from "next/font/google";
 import type { ReactNode } from "react";
+import Script from "next/script";
+
 
 const pixelFont = Press_Start_2P({
   subsets: ["latin"],
@@ -15,7 +17,26 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={pixelFont.className}>{children}</body>
+     <body className={pixelFont.className}>
+
+  {/* Google Analytics */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-VDFLFGL8DN"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VDFLFGL8DN');
+    `}
+  </Script>
+
+  {children}
+
+</body>
+
     </html>
   );
 }
